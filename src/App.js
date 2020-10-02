9a648e34e360157e913de3d36a25e72eb28932ce
andresbr92 <andresbr92@gmail.com>
2020-10-02 17:53:09 +0200

    creating Facebook componets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FacebookLogin from './components/LoginForm/FacebookLogin'
 
 
 function App() {
   return (
     <div className="App">
       <h1> hi</h1>
+      <FacebookLogin />
     </div>
   );
 }
---
+++ src/components/LoginForm/FacebookLogin.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const FacebookLogin = () => {
+    return (
+        <p>facebook login</p>
+    );
+}
+
+export default FacebookLogin;
